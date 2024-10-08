b3bf34645dac96b2857fa714c7892f8810f2efd5
Yohei Fujii <youheifujii624@gmail.com>
2024-10-08 15:33:34 +0900

    コマンドが入力されたことを識別する変数を作り、入力されたときはtrue, それ以外はfalseになるようにした

--- CurlApp.java
+++ CurlApp.java
@@ -5,18 +5,31 @@ import java.net.http.HttpResponse;
 
 public class CurlApp {
     public static void main(String[] args) throws InterruptedException  {
+        //それぞれのコマンドが入力されたかを判定する変数
+        //この変数の値によりサーバーからレスポンスをもらった際の処理を変える
+        boolean specify_req_method = false;
+        boolean specify_data = false;
+        boolean output_file = false;
+        boolean verbose = false;
+
+        //コマンドライン引数をチェック
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-X":
+                    //リクエストメソッドを指定する
+                    specify_req_method = true;
                     System.out.println("コマンドX");
                     break;
                 case "-d":
+                    specify_data = true;
                     System.out.println("コマンドd");
                     break;
                 case "-o":
+                    output_file = true;
                     System.out.println("コマンドo");
                     break;
                 case "-v":
+                    verbose = true;
                     System.out.println("コマンドv");
                     break;
                 default:
