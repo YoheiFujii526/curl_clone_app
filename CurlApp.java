d5b3706a73283a9ecbb86d289d3cc0abc7da8b13
Yohei Fujii <youheifujii624@gmail.com>
2024-10-08 15:23:14 +0900

    コマンドを識別できるようにした

--- CurlApp.java
+++ CurlApp.java
@@ -5,21 +5,42 @@ import java.net.http.HttpResponse;
 
 public class CurlApp {
     public static void main(String[] args) throws InterruptedException  {
-        // ①HttpClientを生成
-        HttpClient cli = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_2)
-            .build();
-        // ②HttpRequestを生成
-        HttpRequest req = HttpRequest.newBuilder()
-            .uri(URI.create("http://abehiroshi.la.coocan.jp/"))
-            .build();
-        // ③リクエストを送信
-        cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
-          .thenAccept(res -> {
-            //bodyの部分はheaders, statusCode, uriに変更できる
-            System.out.println(res.body());
-          });
-         
-        Thread.sleep(3000);
-      }
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-X":
+                    System.out.println("コマンドX");
+                    break;
+                case "-d":
+                    System.out.println("コマンドd");
+                    break;
+                case "-o":
+                    System.out.println("コマンドo");
+                    break;
+                case "-v":
+                    System.out.println("コマンドv");
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+        if (true) {
+            // ①HttpClientを生成
+            HttpClient cli = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+            // ②HttpRequestを生成
+            HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create("http://abehiroshi.la.coocan.jp/"))
+                .build();
+            // ③リクエストを送信
+            cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+            .thenAccept(res -> {
+                //bodyの部分はheaders, statusCode, uriに変更できる
+                System.out.println(res.body());
+            });
+            
+            Thread.sleep(3000);
+        }
+    }
 }
