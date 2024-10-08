05172e4c737a6fb0e79d2a2a39871607da39a027
Yohei Fujii <youheifujii624@gmail.com>
2024-10-08 18:18:20 +0900

    コマンドラインからリクエスト方式をGETかPOSTか選択できるようにした。また、URLも取得できるようにした

--- CurlApp.java
+++ CurlApp.java
@@ -5,6 +5,9 @@ import java.net.http.HttpResponse;
 
 public class CurlApp {
     public static void main(String[] args) throws InterruptedException  {
+
+        //GETかPOST.デフォルトはGET
+        String mode = "GET";
         //それぞれのコマンドが入力されたかを判定する変数
         //この変数の値によりサーバーからレスポンスをもらった際の処理を変える
         boolean specify_req_method = false;
@@ -12,13 +15,25 @@ public class CurlApp {
         boolean output_file = false;
         boolean verbose = false;
 
+        //URL
+        String url = "";
+
         //コマンドライン引数をチェック
         for (int i = 0; i < args.length; i++) {
+            System.out.println(i + "番目の引数" + args[i]);
             switch (args[i]) {
                 case "-X":
                     //リクエストメソッドを指定する
                     specify_req_method = true;
                     System.out.println("コマンドX");
+
+                    //GETかPOSTかを決める
+                    if(i < args.length - 1){
+                        if (args[i+1].equals("GET") || args[i+1].equals("POST")) {
+                            mode = args[i];
+                            System.out.println("モードを決定");
+                        }
+                    }
                     break;
                 case "-d":
                     specify_data = true;
@@ -33,7 +48,13 @@ public class CurlApp {
                     System.out.println("コマンドv");
                     break;
                 default:
-                    
+                    //コマンドに"-"が付いてない
+                    if(!args[i].startsWith("-")) {
+                        if(args[i].startsWith("http://") || args[i].startsWith("https://")) {
+                            url = args[i];
+                            System.out.println("URLを取得:" + url);
+                        }
+                    }
                     break;
             }
         }
@@ -42,16 +63,47 @@ public class CurlApp {
             HttpClient cli = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
-            // ②HttpRequestを生成
-            HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create("http://abehiroshi.la.coocan.jp/"))
-                .build();
-            // ③リクエストを送信
-            cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
-            .thenAccept(res -> {
-                //bodyの部分はheaders, statusCode, uriに変更できる
-                System.out.println(res.body());
-            });
+            
+
+            //-Xコマンドが使われていた場合,モードをGETからPOSTに切り替える
+            if (specify_req_method) {
+                mode = "POST";
+            }
+
+
+            //GETの場合
+            if (mode.equals("GET")) {
+                // ②HttpRequestを生成
+                HttpRequest req = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    //.POST(HttpRequest.BodyPublishers.ofString("some body text"))
+                    .build();
+
+                // ③リクエストを送信
+                cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+                .thenAccept(res -> {
+                    //bodyの部分はheaders, statusCode, uriに変更できる
+                    System.out.println(res.request());
+                });
+
+                
+            //POSTの場合
+            } else if(mode.equals("POST")) {
+                // ②HttpRequestを生成
+                HttpRequest req = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .POST(HttpRequest.BodyPublishers.ofString("some body text"))
+                    .build();
+
+                // ③リクエストを送信
+                cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+                .thenAccept(res -> {
+                    //bodyの部分はheaders, statusCode, uriに変更できる
+                    System.out.println(res.request());
+                });
+            }
+
+            
             
             Thread.sleep(3000);
         }
