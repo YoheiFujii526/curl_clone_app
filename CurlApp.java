a8a230ba0a7b34c1f5ddeaf4d3e9650880c3bf43
Yohei Fujii <youheifujii624@gmail.com>
2024-10-09 15:30:55 +0900

    veerboseをラムダ式内でも使えるようにした

--- CurlApp.java
+++ CurlApp.java
@@ -2,6 +2,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CurlApp {
     public static void main(String[] args) throws InterruptedException  {
@@ -13,14 +14,13 @@ public class CurlApp {
         boolean specify_req_method = false;
         boolean specify_data = false;
         boolean output_file = false;
-        boolean verbose = false;
+        AtomicBoolean verbose = new AtomicBoolean(false);//ラムダ式内で使えるようにする
 
         //URL
         String url = "";
 
         //コマンドライン引数をチェック
         for (int i = 0; i < args.length; i++) {
-            System.out.println(i + "番目の引数" + args[i]);
             switch (args[i]) {
                 case "-X":
                     //リクエストメソッドを指定する
@@ -30,8 +30,7 @@ public class CurlApp {
                     //GETかPOSTかを決める
                     if(i < args.length - 1){
                         if (args[i+1].equals("GET") || args[i+1].equals("POST")) {
-                            mode = args[i];
-                            System.out.println("モードを決定");
+                            mode = args[i+1];
                         }
                     }
                     break;
@@ -44,64 +43,62 @@ public class CurlApp {
                     System.out.println("コマンドo");
                     break;
                 case "-v":
-                    verbose = true;
+                    verbose.set(true);
                     System.out.println("コマンドv");
                     break;
                 default:
                     //コマンドに"-"が付いてない
                     if(!args[i].startsWith("-")) {
+                        //URLかどうかを判定
                         if(args[i].startsWith("http://") || args[i].startsWith("https://")) {
                             url = args[i];
-                            System.out.println("URLを取得:" + url);
                         }
                     }
                     break;
             }
         }
+
+        HttpRequest req  = HttpRequest.newBuilder().uri(URI.create(url)).build();
         if (true) {
             // ①HttpClientを生成
             HttpClient cli = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
-            
-
-            //-Xコマンドが使われていた場合,モードをGETからPOSTに切り替える
-            if (specify_req_method) {
-                mode = "POST";
-            }
-
 
             //GETの場合
             if (mode.equals("GET")) {
                 // ②HttpRequestを生成
-                HttpRequest req = HttpRequest.newBuilder()
+                req = HttpRequest.newBuilder()
                     .uri(URI.create(url))
-                    //.POST(HttpRequest.BodyPublishers.ofString("some body text"))
                     .build();
-
-                // ③リクエストを送信
-                cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
-                .thenAccept(res -> {
-                    //bodyの部分はheaders, statusCode, uriに変更できる
-                    System.out.println(res.request());
-                });
-
-                
             //POSTの場合
             } else if(mode.equals("POST")) {
                 // ②HttpRequestを生成
-                HttpRequest req = HttpRequest.newBuilder()
+                req = HttpRequest.newBuilder()
                     .uri(URI.create(url))
-                    .POST(HttpRequest.BodyPublishers.ofString("some body text"))
+                    //.POST(HttpRequest.BodyPublishers.ofString("some body text"))
+                    .POST(HttpRequest.BodyPublishers.noBody())// データなしでPOSTリクエスト
                     .build();
-
-                // ③リクエストを送信
-                cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
-                .thenAccept(res -> {
-                    //bodyの部分はheaders, statusCode, uriに変更できる
-                    System.out.println(res.request());
-                });
             }
+
+            
+            // ③リクエストを送信
+            //bodyの部分はheaders, statusCode, uriに変更できる
+            cli.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+            .thenAccept(res -> {
+                if (verbose.get()) {
+                    System.out.println("Response Code: " + res.statusCode());
+                    System.out.println("Response Headers: " + res.headers());
+                }
+                //System.out.println(res.statusCode());
+                System.out.println(res.body());
+            });
+            
+            // verboseモードでレスポンス情報を表示
+            /*if (verbose) {
+                System.out.println("Response Code: " + response.statusCode());
+                System.out.println("Response Headers: " + response.headers());
+            }*/
 
             
             
